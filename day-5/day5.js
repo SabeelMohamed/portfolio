4a9000491180bdc341b315dc7e1ec5735c8769f8
SabeelMohamed <153371146+SabeelMohamed@users.noreply.github.com>
2025-01-11 12:51:47 +0530

    Delete day-5 directory

--- day-5/day5.js
+++
@@ -1,6 +0,0 @@
-let age = 26;
-if (age >= 18) {
-    console.log("Eligible to vote");
-} else {
-    console.log("Not eligible");
-}
--- day-5/positive-negative.js
+++
@@ -1,7 +0,0 @@
-let number=-2;
-if(number>0){
-    console.log("positive");
-}
-else{
-    console.log("negative");
-}
